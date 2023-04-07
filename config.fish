5bbd0aa7810e4d6cc43964cc37035fae2fad9ae8
georg <georghirsch97@gmail.com>
2023-04-07 13:56:46 +0200

    initial version

---
+++ config.fish
@@ -0,0 +1,34 @@
+alias batp='cat /sys/class/power_supply/BAT0/capacity'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_prompt
+    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+        set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    end
+    set_color yellow
+    printf '%s' $USER
+    set_color normal
+    printf ' at '
+
+    set_color magenta
+    echo -n (prompt_hostname)
+    set_color normal
+    printf ' in '
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    # Line 2
+    echo
+    if test -n "$VIRTUAL_ENV"
+        printf "(%s) " (set_color blue)(path basename $VIRTUAL_ENV)(set_color normal)
+    end
+    printf '↪ '
+    set_color normal
+end
+function fish_right_prompt
+    printf '%s %s%s' (date +%H:%M) (batp) %
+end
+
