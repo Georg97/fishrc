4cba38e565e54bfd61c58c495f66bd36482b76da
georg <georghirsch97@gmail.com>
2023-04-27 17:42:41 +0200

    updated aliases etc

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,3 +1,6 @@
-alias vimc='cd ~/.config/nvim;nvim .'
-alias fishc='cd ~/.config/fish;nvim .'
+alias vimc='cd ~/.config/nvim;hx .'
+alias fishc='cd ~/.config/fish;hx .'
+alias i3c='cd ~/.config/i3;hx .'
+alias hxc='cd ~/.config/helix;hx .'
+alias signal='flatpak run org.signal.Signal'
 
