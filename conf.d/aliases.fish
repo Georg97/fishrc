84df95c0d2fe3f4682e4767dc9025eb8c6a20540
georg <georghirsch97@gmail.com>
2023-04-28 18:14:42 +0200

    changed config aliases back to nvim

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,6 +1,6 @@
-alias vimc='cd ~/.config/nvim;hx .'
-alias fishc='cd ~/.config/fish;hx .'
-alias i3c='cd ~/.config/i3;hx .'
-alias hxc='cd ~/.config/helix;hx .'
+alias vimc='cd ~/.config/nvim;nvim .'
+alias fishc='cd ~/.config/fish;nvim .'
+alias i3c='cd ~/.config/i3;nvim .'
+alias hxc='cd ~/.config/helix;nvim .'
 alias signal='flatpak run org.signal.Signal'
 
